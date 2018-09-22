8bddf3be61efd52835cadf9a401735c9f7e297f3
Choco0914 <heojeongho1992@gmail.com>
2018-09-22 15:54:33 +0900

    feet: 피자 전문점 홈페이지 피자 전문점 프로젝트에 피자 이름을 보여주는 페이지를 추가했다. 각 피자 이름을 토핑 페이지를 가리키는 링크를 만들었다. 템플릿 상속을 써서 페이지를 효율적으로 만들었다.

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -6,5 +6,10 @@ from . import views
 
 urlpatterns = [
     #홈페이지
-    re_path(r'^$', views.index, name='index')
+    re_path(r'^$', views.index, name='index'),
+
+    # 피자 종류
+    re_path(r'^pizzas/$', views.pizzas, name='pizzas'),
+    # 피자에대한 토핑종류
+    re_path(r'^pizzas/(?P<pizza_id>\d+)/$', views.topping, name='topping')
 ]
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+from .models import Pizza
+
 def index(request):
     """피자 관리 홈페이지"""
     return render(request, 'pizzas/index.html')
+
+def pizzas(request):
+    """pizzz 종류를 나타낸다."""
+    pizzas = Pizza.objects.order_by('date_added')
+    context = {'pizzas' : pizzas}
+    return render(request, 'pizzas/pizzas.html', context)
+
+def topping(request, pizza_id):
+    """pizza종류에 따른 토핑을 나타낸다."""
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by('-date_added')
+    context = {'pizza':pizza, 'toppings': toppings}
+    return render(request, 'pizzas/topping.html', context)
